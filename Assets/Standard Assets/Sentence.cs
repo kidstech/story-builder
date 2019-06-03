1ddbdf647dce5f834083f320f3652fcdeb9a3baa
bella148 <bella148@morris.umn.edu>
2019-06-03 12:37:30 -0500

    Integrate highlighting and bugfix highlighting

--- Assets/Standard Assets/Sentence.cs
+++ Assets/Standard Assets/Sentence.cs
@@ -9,91 +9,114 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Sentence : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class Sentence : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
 
-	// Useful for calling GameObject.Find() reliably
-	public static string sentenceGameObjectName;
+    // Useful for calling GameObject.Find() reliably
+    public static string sentenceGameObjectName;
 
-	// Position of scrollbar at startup
-	public Vector2 defaultScrolledPosition;
+    // Position of scrollbar at startup
+    public Vector2 defaultScrolledPosition;
 
-	/// <summary>
-	/// Initalize object.
-	/// </summary>
-	void Start(){
+    /// <summary>
+    /// Initalize object.
+    /// </summary>
+    void Start()
+    {
 
-		// Grab name for GameObject.Find()
-		sentenceGameObjectName = gameObject.name;
+        // Grab name for GameObject.Find()
+        sentenceGameObjectName = gameObject.name;
 
-		// Save the startup scrolled position
-		defaultScrolledPosition = GetComponent<RectTransform> ().anchoredPosition;
-	}
-		
-	/// <summary>
-	/// Raises the pointer enter event. 
-	/// Helps to determine if a word tile is held within the sentence.
-	/// </summary>
-	/// <param name="eventData">Event data.</param>
-	public void OnPointerEnter (PointerEventData eventData) {
+        // Save the startup scrolled position
+        defaultScrolledPosition = GetComponent<RectTransform>().anchoredPosition;
+    }
 
-		// Grab the possible word tile
-		GameObject wordTile = eventData.pointerDrag;
+    /// <summary>
+    /// Raises the pointer enter event. 
+    /// Helps to determine if a word tile is held within the sentence.
+    /// </summary>
+    /// <param name="eventData">Event data.</param>
+    public void OnPointerEnter(PointerEventData eventData)
+    {
 
-		// A dragged tile is held over the sentence
-		if (wordTile != null && wordTile.GetComponent<WordTile>() != null) 
-			wordTile.GetComponent<WordTile> ().tileHeldOverSentence = true;
+        // Grab the possible word tile
+        GameObject wordTile = eventData.pointerDrag;
 
-	}
+        // A dragged tile is held over the sentence
+        if (wordTile != null && wordTile.GetComponent<WordTile>() != null)
+            wordTile.GetComponent<WordTile>().tileHeldOverSentence = true;
 
-	/// <summary>
-	/// Raises the pointer exit event. 
-	/// Helps to determine if a word tile is held within the sentence.
-	/// </summary>
-	/// <param name="eventData">Event data.</param>
-	public void OnPointerExit (PointerEventData eventData) {
+    }
 
-		// Grab the possible word tile
-		GameObject wordTile = eventData.pointerDrag;
+    /// <summary>
+    /// Raises the pointer exit event. 
+    /// Helps to determine if a word tile is held within the sentence.
+    /// </summary>
+    /// <param name="eventData">Event data.</param>
+    public void OnPointerExit(PointerEventData eventData)
+    {
 
-		// A dragged tile is not held over the sentence
-		if ( wordTile != null && wordTile.GetComponent<WordTile>() != null )
-			wordTile.GetComponent<WordTile> ().tileHeldOverSentence = false;
-		
-	}
+        // Grab the possible word tile
+        GameObject wordTile = eventData.pointerDrag;
 
-	/// <summary>
-	/// Removes all word tiles from the this sentence.
-	/// </summary>
-	public void clear(){
+        // A dragged tile is not held over the sentence
+        if (wordTile != null && wordTile.GetComponent<WordTile>() != null)
+            wordTile.GetComponent<WordTile>().tileHeldOverSentence = false;
 
-		// Reset any scrolling of the transform
-		resetScroll();
+    }
 
-		// Remove all word tiles from the sentence
-		foreach (Transform wordTile in this.transform)
-			Destroy (wordTile.gameObject);
+    /// <summary>
+    /// Removes all word tiles from the this sentence.
+    /// </summary>
+    public void clear()
+    {
 
-	}
+        // Reset any scrolling of the transform
+        resetScroll();
 
-	/// <summary>
-	/// Resets the scroll bar to the default position.
-	/// </summary>
-	public void resetScroll(){
-		GetComponent<RectTransform> ().anchoredPosition = defaultScrolledPosition;
-	}
+        // Remove all word tiles from the sentence
+        foreach (Transform wordTile in this.transform)
+            Destroy(wordTile.gameObject);
 
-	/// <summary>
-	/// Gets the sentence text.
-	/// </summary>
-	/// <returns>The sentence text.</returns>
-	public string getSentenceText(){
-		string phrase = "";
+    }
 
-		// Get all word tile text
-		for(int i = 0; i < transform.childCount; i++)
-			phrase += transform.GetChild (i).GetChild (0).GetComponent<Text> ().text + " ";
-		
-		return phrase;
-	}
+    /// <summary>
+    /// Resets the scroll bar to the default position.
+    /// </summary>
+    public void resetScroll()
+    {
+        GetComponent<RectTransform>().anchoredPosition = defaultScrolledPosition;
+    }
+
+    /// <summary>
+    /// Gets the sentence text.
+    /// </summary>
+    /// <returns>The sentence text.</returns>
+    public string getSentenceText()
+    {
+        string phrase = "";
+
+        // Get all word tile text
+        for (int i = 0; i < transform.childCount; i++)
+            phrase += transform.GetChild(i).GetChild(0).GetComponent<Text>().text + " ";
+
+        return phrase;
+    }
+
+    // ---------High lighting-----------
+
+    public Transform[] getAllTiles()
+    {
+        //Max 32 words in a sentence.
+        Transform[] tiles = new Transform[transform.childCount];
+
+        //For every child on the sentence bar (each tile)
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            tiles[i] = transform.GetChild(i);
+        }
+
+        return tiles;
+    }
 
 }
--- Assets/Standard Assets/TextToSpeechHandler.cs
+++ Assets/Standard Assets/TextToSpeechHandler.cs
@@ -11,87 +11,198 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Crosstales.RTVoice;
 using Crosstales.RTVoice.Model.Event;
+using Crosstales.RTVoice.Model;
+using System.Linq;
 
-public class TextToSpeechHandler : MonoBehaviour {
-
-
-	// Speech button that changes from play to stop
-	public TextToSpeechButton textToSpeechButton;
-
-	// Is a phrase or word currently being spoken?
-	public static bool isSpeaking = false;
-
-	// Are there TTS voices available?
-	public static bool voicesAvailable = true;
-
-	/// <summary>
-	/// Start this instance.
-	/// Checks for voices and initializes event hooks for RTVoice.
-	/// </summary>
-	void Start(){
-
-		// Check if voices are available
-		if (Speaker.Voices.Count <= 0) 
-			voicesAvailable = false;
-		
-		// Initialize TTS event hooks
-		Speaker.OnSpeakStart += speakStartMethod;
-		Speaker.OnSpeakComplete += speakCompleteMethod;
-
-	}
-
-	/// <summary>
-	/// Starts the speaking of the provided phrase.
-	/// </summary>
-	/// <param name="phrase">Phrase to speak.</param>
-	public void startSpeaking(string phrase){
-
-		// If we aren't currently speaking
-		if (!isSpeaking && voicesAvailable) {
-
-			// Then start speaking
-			isSpeaking = true;
-			Speaker.SpeakNative (phrase, Speaker.VoiceForCulture ("en"));
-		}
-
-	}
-		
-	/// <summary>
-	/// Stops the speaking of any speech.
-	/// </summary>
-	public void stopSpeaking(){
-
-		// Stop speaking
-		Speaker.Silence ();
-		isSpeaking = false;
-
-		// Re-enable play option
-		textToSpeechButton.showPlayOption ();
-	}
+public class TextToSpeechHandler : MonoBehaviour
+{
 
 
-	/// <summary>
-	/// Event hook for the start of speaking.
+    // Speech button that changes from play to stop
+    public TextToSpeechButton textToSpeechButton;
+
+    // Is a phrase or word currently being spoken?
+    public static bool isSpeaking = false;
+
+    // Are there TTS voices available?
+    public static bool voicesAvailable = true;
+
+
+    // ----------------- HIGHLIGHTING ---------------
+
+    // Keep track of all the words that WILL be currently spoken.
+    public Transform[] words = new Transform[32];
+
+    // Keep track of the last color the button was
+    public Color prevColor = new Color(255, 255, 0, 100);
+
+    // Keep track on what word in the list we are on
+    public int currentWord = 0;
+
+    // Keep track of the words in a word tile
+    public int tick = 0;
+
+    // Get methods from the sentence box
+    public Sentence sentence;
+
+    /// <summary>
+    /// Start this instance.
+    /// Checks for voices and initializes event hooks for RTVoice.
+    /// </summary>
+    /// 
+
+    private void OnEnable()
+    {
+        Speaker.OnSpeakNativeCurrentWord += SpeakNativeCurrentWord;
+        Speaker.OnSpeakStart += speakStartMethod;
+        Speaker.OnSpeakComplete += speakCompleteMethod;
+    }
+
+    void Start()
+    {
+
+        // Check if voices are available
+        if (Speaker.Voices.Count <= 0)
+            voicesAvailable = false;
+
+        // Initialize TTS event hooks
+
+    }
+
+    /// <summary>
+    /// Starts the speaking of the provided phrase.
+    /// </summary>
+    /// <param name="phrase">Phrase to speak.</param>
+    public void startSpeaking(string phrase)
+    {
+
+        // If we aren't currently speaking
+        if (!isSpeaking && voicesAvailable)
+        {
+
+            // Then start speaking
+            isSpeaking = true;
+            Speaker.SpeakNative(phrase, Speaker.VoiceForCulture("en"));
+        }
+
+    }
+
+    /// <summary>
+    /// Stops the speaking of any speech.
+    /// </summary>
+    public void stopSpeaking()
+    {
+
+        // Stop speaking
+        Speaker.Silence();
+        isSpeaking = false;
+
+        // Re-enable play option
+        textToSpeechButton.showPlayOption();
+
+        // ------HIGHLIGHTING-------
+        // Reset Color
+        words[currentWord - 1].GetComponent<Image>().color = prevColor;
+
+        // Reset Variables
+        currentWord = 0;
+        tick = 0;
+    }
+
+
+    /// <summary>
+    /// Event hook for the start of speaking.
+    /// </summary>
+    /// <param name="e">E.</param>
+    private void speakStartMethod(SpeakEventArgs e)
+    {
+        // Allow user to stop speech
+        textToSpeechButton.showStopOption();
+
+
+        // ------HIGHLIGHTING-------
+        // Reset word counter
+        currentWord = 0;
+
+        // Populate the array of Words
+        words = sentence.getAllTiles();
+    }
+
+    /// <summary>
+    /// Event hook for the completion of speaking.
+    /// </summary>
+    /// <param name="e">E.</param>
+    private void speakCompleteMethod(SpeakEventArgs e)
+    {
+
+        // No longer speaking
+        isSpeaking = false;
+
+        // Allow user to use TTS again
+        textToSpeechButton.showPlayOption();
+
+        // ------HIGHLIGHTING-------
+
+        // Set the last word back to its original color
+        if(currentWord > 0)
+        {
+            words[currentWord - 1].GetComponent<Image>().color = prevColor;
+        }
+        else
+        {
+            words[0].GetComponent<Image>().color = prevColor;
+        }
+
+        // Reset Word Counter
+        currentWord = 0;
+    }
+
+    /// <summary>
+	/// Event hook for speaking current word.
 	/// </summary>
 	/// <param name="e">E.</param>
-	private void speakStartMethod(SpeakEventArgs e){
+    private void SpeakNativeCurrentWord(SpeakEventArgs e)
+    {
+        // ------HIGHLIGHTING-------
 
-		// Allow user to stop speech
-		textToSpeechButton.showStopOption ();
-	}
+        // 'Tick' represents the number words in a tile.
+        if (tick == 0)
+        {
+            // Get the text in the tile and count the number of spaces in it.
+            tick = words[currentWord].GetComponentInChildren<Text>().text.Count(char.IsWhiteSpace);
 
-	/// <summary>
-	/// Event hook for the completion of speaking.
-	/// </summary>
-	/// <param name="e">E.</param>
-	private void speakCompleteMethod(SpeakEventArgs e){
+            // Set the previous tile back to the color it was
+            if (currentWord > 0)
+            {
+                words[currentWord - 1].GetComponent<Image>().color = prevColor;
+            }
 
-		// No longer speaking
-		isSpeaking = false;
+            // Save the color of the n'th tile
+            prevColor = words[currentWord].GetComponent<Image>().color;
 
-		// Allow user to use TTS again
-		textToSpeechButton.showPlayOption ();
-	}
+            // Change the color of the n'th tile to the highlighted color
+            words[currentWord].GetComponent<Image>().color = new Color32(255, 255, 0, 100);
+
+            // If the tick is still equal to 0, then that means there is no spaces or anything, and is a single word. We can move to the next word safely.
+            if(tick == 0)
+            {
+                currentWord++;
+            }
+
+        }
+        else
+        {
+            // Incement Word if tick is going to become zero
+            if (tick == 1)
+            {
+                currentWord++;
+            }
+
+            // Count down a tick
+            tick--;
+        }
+
+
+    }
 
 }
-
