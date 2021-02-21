7fc8cacfd12b8a401d12af0130b3038ec7f65996
Ben Burgess <burge337@morris.umn.edu>
2021-02-21 03:06:47 -0600

    Create word tile image for the submitted sentences
    I've also made it so that the sentence tile is above everything else, with the exception of the sentence view. Because of some funky behavior with eventData.hovered, the dragged object needs to be below the sentence view in order to actually detect it. This is probably because the dragged object blocks raycasts otherwise, but I'm still looking into it.

--- Assets/Scenes/Sentence Builder/Lever/SaveSentenceTiles.cs
+++ Assets/Scenes/Sentence Builder/Lever/SaveSentenceTiles.cs
@@ -6,33 +6,53 @@ using UnityEngine.EventSystems;
 public class SaveSentenceTiles : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public static GameObject draggedObject;
+    int startSibIndex;
     private Vector3 startPosition;
     public List<WordTile> savedSentence; // storage for the latest submitted word tiles in case the user wishes to modify their sentence
     // Start is called before the first frame update
     private bool dragging;
+    private GameObject sentence = null;
+    private bool drop = false;
     
+    public void Start()
+    {
+        sentence = GameObject.Find("Sentence");
+    }    
+
+    // seems like we'll need to stop blocking raycasts on the object we are dragging so that we can actually detect stuff beneath it
     public void OnBeginDrag(PointerEventData eventData)
     {
         draggedObject = gameObject;
         startPosition = transform.position;
+        startSibIndex = transform.GetSiblingIndex();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        // Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
         transform.position = Input.mousePosition;
+        // if(eventData.hovered.Contains(sentence)){
+        //     drop = true; //hovering over the sentence view, we want to resubmit the sentence
+        //     transform.SetAsLastSibling();// ensure the dragged object is over the sentence view
+        // } else {
+        //     drop = false;
+        //     transform.SetSiblingIndex(startSibIndex); // drop the dragged object back below the sentence view so that eventData.hovered can still detect it
+        // }
     }
 
+    // There's a bit of a weird quirk of eventData.hovered here, in that it seems to only work in finding objects that are below what we are dragging in the hierarchy.
+    // Because of this sentence resubmission will break if SentenceScrollview ever finds its way above MostRecentSavedSentence
     public void OnEndDrag(PointerEventData eventData)
     {
-        GameObject sentence = GameObject.Find("Sentence");
         // if we drag and release our sentence over the sentence bar, resubmit it
         if (eventData.hovered.Contains(sentence))
         {
-            resubmitSentence(draggedObject.GetComponent<SaveSentenceTiles>().savedSentence);
+            resubmitSentence(draggedObject.GetComponentInChildren<SaveSentenceTiles>().savedSentence);
         }
         // reset the submitted sentence object back to its original position
         draggedObject = null;
         transform.position = startPosition;
+        transform.SetSiblingIndex(startSibIndex); // reset parent so dragged object is no longer on top of everything
     }
 
     // note: with current implementation, the resubmitted word tiles cannot be placed in between word tiles in the sentence bar. They will always append themselves to the end.
