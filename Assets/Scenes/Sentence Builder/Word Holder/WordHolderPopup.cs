e987f9c0fdda1bec576acd43391ccb420cd5accc
Ben Burgess <burge337@morris.umn.edu>
2021-04-18 03:52:07 -0500

    Create initial "standard list" version of the word holder popup
    The word holder now lists the word forms in rows of 3, rather than in a circle around the middle word.
    
    Changes target issue #64

--- Assets/Scenes/Sentence Builder/Word Holder/WordHolderPopup.cs
+++ Assets/Scenes/Sentence Builder/Word Holder/WordHolderPopup.cs
@@ -24,9 +24,11 @@ public class WordHolderPopup : MonoBehaviour
     {
         // Middle Word
         GameObject midButton = Instantiate(WordHolderPopupPrefabButton);
+        float buttonWidth = midButton.GetComponent<RectTransform>().rect.width;
+        float buttonHeight = midButton.GetComponent<RectTransform>().rect.height;
 
         //
-        midButton.GetComponent<RectTransform>().localPosition = new Vector2(0, 0);
+        midButton.GetComponent<RectTransform>().localPosition = new Vector2(0, 2*buttonHeight); // top middle
         midButton.GetComponent<Image>().color = Color.yellow;
         midButton.GetComponent<Button>().interactable = false;
         midButton.GetComponentInChildren<Text>().text = word.word;
@@ -41,26 +43,51 @@ public class WordHolderPopup : MonoBehaviour
         //
         buttons.Add(midButton);
 
+
+        /**
+        Note: All this colNumber and rowNumber stuff should probably just be refactored to use prefabs and a gridlayout group like the wordbank eventually if we end up keeping this layout
+        **/
+        int colNumber = 1; 
+        int rowNumber = 1;
+        float topRowPosition = midButton.GetComponent<RectTransform>().localPosition.y;
         //
         for (int i = 0; i < word.forms.Count; i++)
         {
             //
             GameObject button = Instantiate(WordHolderPopupPrefabButton);
-
-            //
-            float offset = i * 2 * Mathf.PI / word.forms.Count + Mathf.PI / 2;
-
-            //
-            button.GetComponent<RectTransform>().localPosition = new Vector2(200 * Mathf.Cos(offset), 200 * Mathf.Sin(offset));
-
             //
             button.GetComponentInChildren<Text>().text = word.forms[i];
-
             //
             button.transform.SetParent(this.transform, false);
 
             //
             buttons.Add(button);
+
+            // positioning word form buttons in rows of 3 starting from the top left
+            if (colNumber == 1)
+            {
+                button.GetComponent<RectTransform>().localPosition = new Vector2(-buttonWidth, topRowPosition - rowNumber * buttonHeight);
+            }
+            else if (colNumber == 2)
+            {
+                button.GetComponent<RectTransform>().localPosition = new Vector2(0, topRowPosition - rowNumber * buttonHeight);
+            }
+            else // col number is 3
+            {
+                button.GetComponent<RectTransform>().localPosition = new Vector2(buttonWidth, topRowPosition - rowNumber * buttonHeight);
+            }
+
+            /* cloud based word form list positioning
+            float offset = i * 2 * Mathf.PI / word.forms.Count + Mathf.PI / 2;
+            button.GetComponent<RectTransform>().localPosition = new Vector2(200 * Mathf.Cos(offset), 200 * Mathf.Sin(offset));
+            */
+
+            if (colNumber < 3) colNumber++;
+            else // we've made 3 columns. switch to new row and reset column count
+            {
+                colNumber = 1; 
+                rowNumber++;
+            }
         }
 
         //
