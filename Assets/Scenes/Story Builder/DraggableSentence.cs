2a6d858a09900e509df137e7d6d20924ff55f67e
Ben Burgess <burge337@morris.umn.edu>
2021-06-05 20:54:06 -0500

    Fix sentence duplication bug in Storybuilder
    Fixes an issue where dragging a sentence tile out of the sentence bank would leave its copy at the bottom of the sentence bank. There now exists a game object called "DefaultArea" that has the sentencebankdropzone script attached to it with default behavior selected that when hovered over will properly update the dragged sentence's behavior.
    
    Also removed empty comments from a few files.

--- Assets/Scenes/Story Builder/DraggableSentence.cs
+++ Assets/Scenes/Story Builder/DraggableSentence.cs
@@ -15,122 +15,90 @@ public class DraggableSentence : MonoBehaviour, IBeginDragHandler, IDragHandler,
     [HideInInspector]
     public GameObject placeholder = null;
 
-    //
     public SentenceDropzone.Behavior heldOver;
     public SentenceDropzone.Behavior draggedFrom = SentenceDropzone.Behavior.SentenceBank;
 
-    //
     private Transform storyBuilderCanvas;
 
-    //
     private Vector2 offset = Vector2.zero;
 
-    //
     private CanvasGroup canvasGroup = null;
 
-    //
     private void Start()
     {
         storyBuilderCanvas = GameObject.Find("StoryBuilderCanvas").transform;
-        //
         canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    //
     public void OnBeginDrag(PointerEventData eventData)
     { 
-        //
         offset = this.transform.position - Input.mousePosition;
 
-        //
         placeholder = new GameObject();
         placeholder.transform.SetParent(this.transform.parent);
 
-        //
         LayoutElement le = placeholder.AddComponent<LayoutElement>();
         le.preferredWidth = this.transform.parent.GetComponent<GridLayoutGroup>().cellSize.x;
         le.preferredHeight = this.transform.parent.GetComponent<GridLayoutGroup>().cellSize.y;
         le.flexibleWidth = 0;
         le.flexibleHeight = 0;
 
-        //
         if(draggedFrom == SentenceDropzone.Behavior.SentenceBank)
         {
-            //
             GameObject o = Instantiate(this.gameObject);
 
-            //
             o.GetComponent<SentenceObject>().savedSentence = this.gameObject.GetComponent<SentenceObject>().savedSentence;
 
-            //
             o.transform.SetParent(this.transform.parent, false);
             o.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
 
-            //
             placeholder.transform.SetAsLastSibling();
         }
         else
         {
-            //
             placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
         }
 
-        //
         parentToReturnTo = this.transform.parent;
 
-        //
         placeholderParent = parentToReturnTo;
 
         // Set this parent as the canvas
         this.transform.SetParent(storyBuilderCanvas);
 
-        //
         canvasGroup.blocksRaycasts = false;
     }
 
-    //
     public void OnDrag(PointerEventData eventData)
     {
-        //
         //this.transform.position = (eventData.position + offset);
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = Camera.main.nearClipPlane;
         this.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
-        //
         if (heldOver == SentenceDropzone.Behavior.Page)
         {
-            //
             if (placeholder.transform.parent != placeholderParent)
             {
-                //
                 placeholder.transform.SetParent(placeholderParent, false);
             }
 
-            //
             int newSiblingIndex = placeholderParent.childCount;
 
-            //
             for (int i = 0; i < placeholderParent.childCount; i++)
             {
-                //
                 if (this.transform.position.y > placeholderParent.GetChild(i).position.y)
                 {
-                    //
                     newSiblingIndex = i;
 
-                    //
                     if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
                     {
-                        //
                         newSiblingIndex--;
                     }
 
-                    //
                     break;
                 }
             }
 
-            //
             placeholder.transform.SetSiblingIndex(newSiblingIndex);
         }
         else
@@ -138,29 +106,23 @@ public class DraggableSentence : MonoBehaviour, IBeginDragHandler, IDragHandler,
             // Remove the placeholder from the sentencebank
             if (placeholder.transform.parent != placeholderParent)
             {
-                //
                 placeholder.transform.SetParent(placeholderParent, false);
             }
         }
     }
 
-    //
     public void OnEndDrag(PointerEventData eventData)
     {
-        //
         this.transform.SetParent(parentToReturnTo, false);
         this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
         RectTransform sentenceTileCoords = this.transform.GetComponent<RectTransform>();
         // give the sentence tile a draggable z position
         this.transform.GetComponent<RectTransform>().position = new Vector3(sentenceTileCoords.position.x, sentenceTileCoords.position.y, 0);
 
-        //
         draggedFrom = parentToReturnTo.GetComponent<SentenceDropzone>().behavior;
 
-        //
         canvasGroup.blocksRaycasts = true;
 
-        //
         Destroy(placeholder);
     }
 }
