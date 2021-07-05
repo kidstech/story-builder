46bfae18fae1adbc75613dd5a5866bf98e649b92
Ben Burgess <burge337@morris.umn.edu>
2021-07-04 19:42:51 -0500

    Add a button that goes back to the learner select screen
    Also changed most (all?) of the text fonts of buttons and sliders in the app the the dyslexia font.
    
    Targets issue #62

---
+++ Assets/Scenes/Main Menu/Scripts/ChangeLearnerButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLearnerButton : MonoBehaviour
+{
+    public void GoToLearnerLoginScene()
+    {
+        StartCoroutine(LearnerSelect());
+    }
+    public IEnumerator LearnerSelect()
+    {
+        AsyncOperation sceneChange = SceneManager.LoadSceneAsync(3, LoadSceneMode.Single);
+        while (!sceneChange.isDone)
+        {
+            yield return null;
+        }
+    }
+}
