221ed75d32efa6eb663dc8e2badc75de1278c436
Ben Burgess <burge337@morris.umn.edu>
2021-08-04 17:40:54 -0500

    Fix change user not working if a learner hadn't signed in yet
    Also includes minor improvements to options menu UI looks and behavior:
    1) Close options menu button is now a big red X at the top right corner
    2) Update Word Bank now also closes the menu
    
    TODO: add "wordbank updated" message to the update word bank button so the user knows if it actually worked.

--- Assets/Scenes/Main Menu/Scripts/OptionsMenuHandler.cs
+++ Assets/Scenes/Main Menu/Scripts/OptionsMenuHandler.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class OptionsMenuHandler : MonoBehaviour
 {
+    [Header("GameObjectReferences")]
     public GameObject optionsPanel;
+    // this button is not a child of the options panel so that it can remain at the top right in spite of layout group properties
+    public GameObject closeMenuButton;
     public void openMenu()
     {
+        closeMenuButton.SetActive(true);
         optionsPanel.SetActive(true);
     }
     public void closeMenu()
     {
+        closeMenuButton.SetActive(false);
         optionsPanel.SetActive(false);
     }
 }
