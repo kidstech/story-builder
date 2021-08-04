dd1c93675b3d334bd43da2835c7ea3f46813f639
Ben Burgess <burge337@morris.umn.edu>
2021-08-04 17:24:04 -0500

    Fix StoryBuilder arrow buttons
    Also removed a page deletion confirmation prompt that only works in editor mode.

--- Assets/Scenes/Story Builder/DeletePageButton/DeletePageButton.cs
+++ Assets/Scenes/Story Builder/DeletePageButton/DeletePageButton.cs
@@ -14,20 +14,20 @@ public class DeletePageButton : MonoBehaviour
     private void Start()
     {
         button = GetComponent<Button>();
-        button.onClick.AddListener(ShowDialog);
+        //button.onClick.AddListener(ShowDialog);
     }
 
-    private void DeletePage()
+    public void DeletePage()
     {
         pageIconContainer.RemovePage();
     }
 
     // pop up confirmation window to confirm deletion
-    private void ShowDialog() 
-    {
-        if (EditorUtility.DisplayDialog("Confirm Deletion", "Are you sure you want to delete this page?", "Delete", "Cancel") == true)
-        {
-            DeletePage();
-        }
-    }
+    // private void ShowDialog() 
+    // {
+    //     if (EditorUtility.DisplayDialog("Confirm Deletion", "Are you sure you want to delete this page?", "Delete", "Cancel") == true)
+    //     {
+    //         DeletePage();
+    //     }
+    // }
 }
