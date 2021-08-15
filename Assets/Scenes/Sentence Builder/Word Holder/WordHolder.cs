fb90ef58d00de57b8fdfb618fd3a55cd4039324b
Ben Burgess <burge337@morris.umn.edu>
2021-08-14 20:30:03 -0500

    Remove now irrelevant comment

--- Assets/Scenes/Sentence Builder/Word Holder/WordHolder.cs
+++ Assets/Scenes/Sentence Builder/Word Holder/WordHolder.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,16 +9,10 @@ public class WordHolder : MonoBehaviour
     [SerializeField]
     private Transform SentenceBuilderCanvas;
 
-    //
     public void OpenWordHolder(Word word)
     {
-        //
         GameObject popup = Instantiate(WordHolderPopupPrefab);
-
-        //
         popup.GetComponent<WordHolderPopup>().SetupWordHolderPopup(word);
-
-        // Set as a child of the canvas (WordHolderDrop -> WordHolder -> Canvas)
         popup.transform.SetParent(SentenceBuilderCanvas, false);
     }
 }
