146255abc632bf572b03ef0baaac5768e8c6017b
Ben Burgess <burge337@morris.umn.edu>
2021-10-31 19:31:50 -0500

    Fix word holder and null reference error
    At some point the wordholder drop game object had gotten over top of the wordholder game object and was seemingly not receiving raycasts because of this. Wordholder drop was moved below wordholder and the issue was fixed. 
    
    Also includes some comments for a couple different methods.

--- Assets/Scenes/Sentence Builder/Filtering/Packs/SetupPackFilter.cs
+++ Assets/Scenes/Sentence Builder/Filtering/Packs/SetupPackFilter.cs
@@ -39,13 +39,17 @@ public class SetupPackFilter : MonoBehaviour
                 if (pack.icon != "" && pack.icon != null)
                 {
                     iconCount++;
-                    //Debug.Log("iconCount is: " + iconCount);
+                    Debug.Log("iconCount is: " + iconCount);
                     // then we query firebase and store them
                     GetPackIconAndStoreLocally(pack);
+
                 }
             }
-            // call needed to ensure we actually still set things up if we don't find any icons
-            SetUpContextPackSortButtons();
+            if (iconCount == 0)
+            {
+                // call needed to ensure we actually still set things up if we don't find any icons
+                SetUpContextPackSortButtons();
+            }
         }
         else
         {
@@ -60,17 +64,21 @@ public class SetupPackFilter : MonoBehaviour
     }
 
 
-    // This method stores icons grabbed from firebase.
-    // When the class' icon count reaches 0, it has finished all the server requests it started and will
-    // then setup the context pack sort buttons
+    /// <summary>This method stores icons grabbed from firebase. <br/>
+    /// When the class' icon count reaches 0, it has finished all the server requests it started and will
+    /// then setup the context pack sort buttons </summary>
+    /// <param name="icon"> A byte array containing the data for a context pack icon </param>
+    /// <param name="id"> The mongo object id of the context pack associated with the given icon byte array </param>
     private void StorePackIconLocally(byte[] icon, string id)
     {
+        Debug.Log("storing context pack locally");
+        ContextPackHandler.StoreContextPackIcon(id, icon);
+        iconCount--;
         if (iconCount <= 0)
         {
             Debug.Log("firebase requests finished... setting up context pack buttons");
             SetUpContextPackSortButtons();
         }
-        iconCount--;
         
     }
 
@@ -86,17 +94,16 @@ public class SetupPackFilter : MonoBehaviour
             sortButton.name = filterByPacks[i].name;
 
             //
-            sortButton.GetComponentInChildren<Text>().text = filterByPacks[i].name;
-
-            /*
-             * Need to try to load an image from the path above ^ 
-             */
             if (filterByPacks[i]?.icon != "" )
             {
                 Sprite packSprite = ContextPackHandler.GetContextPackIconFromStorage(filterByPacks[i]._id);
                 AssignSprite(packSprite);
+                sortButton.GetComponentInChildren<Text>().text = ""; // remove text if we have an icon for a pack
             }
-
+            else{
+                sortButton.GetComponentInChildren<Text>().text = filterByPacks[i].name;
+            }
+            
             //
             sortButton.AddComponent<PackFilterButton>().pack = filterByPacks[i];
 
@@ -105,8 +112,15 @@ public class SetupPackFilter : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Stores the given sprite in the sort button prefab image component <br/>
+    /// </summary>
+    /// <param name="sprite"> Unity sprite to be assigned to an image component </param>
+    /// <return> void </return>
     private void AssignSprite(Sprite sprite)
     {
+        // WIP for getting sprites with rounded edges 
+        //Sprite borderedSprite = Sprite.Create(sprite.texture, new Rect(0,0,40,30), new Vector2(0,0), 1, 0, SpriteMeshType.Tight, new Vector4(0,0,0,0));
         sortButton.GetComponent<Image>().sprite = sprite;
     }
 }
--- Assets/Scenes/Shared Scenes/SessionData/ServerRequestHandler.cs
+++ Assets/Scenes/Shared Scenes/SessionData/ServerRequestHandler.cs
@@ -8,6 +8,7 @@ using System;
 using DatabaseEntry;
 public class ServerRequestHandler : MonoBehaviour
 {
+    // DON'T COMMIT THIS UNLESS YOU'VE REMOVED YOUR IP
     private static readonly string serverIp = "http://localhost:4567"; // change localhost to ip of target machine if using separate device
     public static IEnumerator GetLearnerIconFromFirebase(Learner learner, Action<Learner> action)
     {
