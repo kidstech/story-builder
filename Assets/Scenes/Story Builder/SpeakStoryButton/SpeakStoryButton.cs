0ad366155a2ec0a0a4c1baf667c9e598e53eb8d4
Joshua Eklund <eklun124@umn.edu>
2022-12-27 17:02:25 -0600

    Rename SpeakPageButton to SpeakStoryButton
    This was done as pages no longer exist.

--- Assets/Scenes/Story Builder/SpeakStoryButton/SpeakStoryButton.cs
+++ Assets/Scenes/Story Builder/SpeakStoryButton/SpeakStoryButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpeakPageButton : MonoBehaviour
+public class SpeakStoryButton : MonoBehaviour
 {
     public SavedSentenceBank sentenceBank;
     private Button button;
