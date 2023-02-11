4e0d8a0a3724082ba8da64be8c8fc2833e262c71
Joshua Eklund <eklun124@umn.edu>
2023-02-11 16:22:43 -0600

    Disable speak sentence button while context pack menu is open
    This commit prevents the speak sentence button from blocking raycasts while the context pack menu is open in order to prevent   potential misclicks.

--- Assets/Scenes/Sentence Builder/Filtering/Packs/ContextPackMenu.cs
+++ Assets/Scenes/Sentence Builder/Filtering/Packs/ContextPackMenu.cs
@@ -7,12 +7,26 @@ public class ContextPackMenu: MonoBehaviour {
 
     [SerializeField]
     private Button closeMenu;
+    
+    [SerializeField]
+    private Image speakSentence;
 
 
     void Start() {
         closeMenu.onClick.AddListener(()=> {
             this.gameObject.SetActive(false);
         });
+    }
+
+    /*
+    * Prevents the speak sentence button from being able to be pressed while the context pack menu is open to prevent potential misclicks
+    */
+    void OnEnable() {
+        speakSentence.raycastTarget = false;
+    }
+    
+    void OnDisable() {
+        speakSentence.raycastTarget = true;
     }
 
 }
